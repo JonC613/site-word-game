b540a4d8735f221418cbe2adbb43d512927b24b5
JonC613 <jonchester@gmail.com>
2025-02-22 13:47:02 -0600

    refactor: remove unused useCallback import from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { PrimaryButton, DefaultButton, Stack, Text, IStackStyles, ITextStyles, IButtonStyles } from '@fluentui/react';
 import siteWordsData from './data/sight_words.json';
 
